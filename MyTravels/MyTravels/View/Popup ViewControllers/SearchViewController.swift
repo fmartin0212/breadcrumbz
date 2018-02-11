1be965e002f452af7a5f5491b99a26b196da95cf
fmartin0212 <fmartin0212@gmail.com>
2018-02-11 15:49:39 -0700

    Add SearchViewController to search using user's location

--- MyTravels/MyTravels/View/Popup ViewControllers/SearchViewController.swift
+++ MyTravels/MyTravels/View/Popup ViewControllers/SearchViewController.swift
@@ -7,13 +7,100 @@
 //
 
 import UIKit
+import MapKit
 
 class SearchViewController: UIViewController {
 
+    // MARK: - Constants & Variables
+    var searchCompleter = MKLocalSearchCompleter()
+    var searchResults = [MKLocalSearchCompletion]()
+    
+    // MARK: - IBOutlets
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        searchCompleter.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        searchCompleter.queryFragment = searchText
+    }
+}
+
+
+extension SearchViewController: MKLocalSearchCompleterDelegate {
+    
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        searchResults = completer.results
+        tableView.reloadData()
+}
+    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+        // handle error
+    }
+    
+    func highlightedText(_ text: String, inRanges ranges: [NSValue], size: CGFloat) -> NSAttributedString {
+        let attributedText = NSMutableAttributedString(string: text)
+        let regular = UIFont.systemFont(ofSize: size)
+        attributedText.addAttribute(NSAttributedStringKey.font, value:regular, range:NSMakeRange(0, text.count))
+        
+        let bold = UIFont.boldSystemFont(ofSize: size)
+        for value in ranges {
+            attributedText.addAttribute(NSAttributedStringKey.font, value:bold, range:value.rangeValue)
+        }
+        return attributedText
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let searchResult = searchResults[indexPath.row]
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        cell.textLabel?.attributedText = highlightedText(searchResult.title, inRanges: searchResult.titleHighlightRanges, size: 17.0)
+        cell.detailTextLabel?.attributedText = highlightedText(searchResult.subtitle, inRanges: searchResult.subtitleHighlightRanges, size: 12.0)
+        return cell
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let completion = searchResults[indexPath.row]
+        
+        let searchRequest = MKLocalSearchRequest(completion: completion)
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { (response, error) in
+            
+            guard let response = response else { return }
+            
+            let coordinate = response.mapItems[0].placemark.coordinate
+            let longitude = coordinate.longitude
+            let latitude = coordinate.latitude
+            
+            print(String(describing: coordinate))
+//            self.delegate?.updateLongLat(long: longitude, lat: latitude)
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
