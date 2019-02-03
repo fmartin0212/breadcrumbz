7e0972ef75c9ae6aca7a27f4a40a23326ee24936
fmartin0212 <fmartin0212@gmail.com>
2019-02-02 18:27:34 -0700

    jenkins PR test

--- MyTravels/Resources/AppDelegate.swift
+++ MyTravels/Resources/AppDelegate.swift
@@ -12,7 +12,7 @@ import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-// my change
+// my test commit
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
