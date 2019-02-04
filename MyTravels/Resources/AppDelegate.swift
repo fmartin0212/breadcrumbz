5d5aa7dca51efbc93a0e924e67c0224f6ebf3dfe
fmartin0212 <fmartin0212@gmail.com>
2019-02-04 07:15:03 -0700

    another random change

--- MyTravels/Resources/AppDelegate.swift
+++ MyTravels/Resources/AppDelegate.swift
@@ -12,7 +12,7 @@ import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-// my change
+// my change another change
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
